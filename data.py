a68d388bcc5bf61cf528466d1c2a11ef43b0b466
azam2020 <azam8933033@gmail.com>
2024-01-06 23:33:12 +0530

    code

--- data.py
+++ data.py
@@ -2,12 +2,12 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 skip = 0
 face_data = []
 
-dataset_path = "/face_dataset/"
+dataset_path = "./face_dataset/"
 file_name = input("Enter the name of the person: ")
 
 while True:
@@ -18,13 +18,15 @@ while True:
 
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
 
     if len(faces) == 0:
         continue
+        
+    k=1
 
     faces = sorted(faces, key=lambda x: x[2] * x[3], reverse=True)
-    skip = 1
+    skip += 1
 
     for face in faces[:1]:
         x, y, w, h = face
@@ -33,25 +35,26 @@ while True:
         face_selection = cv2.resize(face_offset, (100, 100))
 
         if skip % 10 == 0:
-            face_data.append(face_selection)
-
-        print(len(face_data))
+            face_data.append(face_selection.flatten())
+            print(len(face_data))
 
         cv2.imshow(str(skip), face_selection)
-        skip += 1
+        k += 1
 
         cv2.rectangle(frame, (x, y), (x + w, y + h), (8, 255, 0), 2)
 
     cv2.imshow("Faces", frame)
 
     key_pressed = cv2.waitKey(1) & 0xFF
-    if key_pressed == ord('a'):
+    if key_pressed == ord('q'):
         break
 
-face_data = np.asarray(face_data)
+face_data = np.array(face_data)
+
+face_data = face_data.reshape((face_data.shape[0],-1))
 print(face_data.shape)
 
 np.save(dataset_path + file_name, face_data)
-
+print("Dataset saved")
 cap.release()
 cv2.destroyAllWindows()
--- label.py
+++ label.py
@@ -11,22 +11,25 @@ def load_dataset(dataset_path):
         if filename.endswith(".npy"):
             data = np.load(os.path.join(dataset_path, filename))
             label = filename[:-4]  # Remove the '.npy' extension
+            #data_flattened = data.flatten()
             face_data.append(data)
             labels.extend([label] * len(data))
-
+		
     face_data = np.concatenate(face_data, axis=0)
+    #print(face_data)
     return face_data, labels
 
 def predict_person(face_data, labels, test_face):
     knn_classifier = KNeighborsClassifier(n_neighbors=3)
+    test_face_flattened = test_face.flatten()
     knn_classifier.fit(face_data, labels)
-    predicted_label = knn_classifier.predict([test_face])[0]
+    predicted_label = knn_classifier.predict([test_face_flattened])[0]
     return predicted_label
 
 cap = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-dataset_path = "/face_dataset/"
+dataset_path = "./face_dataset/"
 
 face_data, labels = load_dataset(dataset_path)
 
@@ -38,7 +41,7 @@ while True:
 
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
 
     if len(faces) == 0:
         continue
@@ -48,16 +51,16 @@ while True:
         x, y, w, h = face
         face_offset = frame[y:y+h, x:x+w]
         test_face = cv2.resize(face_offset, (100, 100))
-
+        
         predicted_label = predict_person(face_data, labels, test_face)
-
+        
         cv2.rectangle(frame, (x, y), (x + w, y + h), (8, 255, 0), 2)
         cv2.putText(frame, predicted_label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2, cv2.LINE_AA)
 
     cv2.imshow("Face Recognition", frame)
-
+    
     key_pressed = cv2.waitKey(1) & 0xFF
-    if key_pressed == ord('a'):
+    if key_pressed == ord('q'):
         break
 
 cap.release()
