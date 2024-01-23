bd8bfcdab77590338d83d623272254eb634b6e50
azam2020 <azam8933033@gmail.com>
2024-01-23 11:40:33 +0530

    up

--- img_server.py
+++ img_server.py
@@ -25,9 +25,9 @@ def img_face(temp_path):
 	r = h/w
 	dh = int(r*dw)
 	img = cv2.resize(img,(dw,dh))
-	cv2.imshow('Image',img)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+	#cv2.imshow('Image',img)
+	#cv2.waitKey(0)
+	#cv2.destroyAllWindows()
 
 def video_face():
 	print("video-face function called")
